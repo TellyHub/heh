788ffeaa51b0e1335ab1f6f583c68e05c4f2314e
TellyHub <106040852+TellyHub@users.noreply.github.com>
2022-07-06 13:56:46 +0530

    Update echo.py

--- plugins/echo.py
+++ plugins/echo.py
@@ -178,49 +178,4 @@ async def echo(bot, update):
                 approx_file_size = ""
                 if "filesize" in formats:
                     approx_file_size = humanbytes(formats["filesize"])
-                cb_string = "{}|{}|{}".format(
-                    "video", format_id, format_ext, randem)
-                if not "audio only" in format_string:
-                    ikeyboard = [
-                        InlineKeyboardButton(
-                            "[" + format_string +
-                            "] (" + format_ext + " - " +
-                            approx_file_size + ")",
-                            callback_data=(cb_string).encode("UTF-8")
-                        )
-                    ]
-                    inline_keyboard.append(ikeyboard)
-            cb_string = "{}|{}|{}".format("audio", "5", "mp3")
-            inline_keyboard.append([
-                InlineKeyboardButton(
-                    "MP3 " + "(" + "medium" + ")", callback_data=cb_string.encode("UTF-8"))
-            ])
-            cb_string = "{}|{}|{}".format("audio", "0", "mp3")
-            inline_keyboard.append([
-                InlineKeyboardButton(
-                    "MP3 " + "(" + "best" + ")", callback_data=cb_string.encode("UTF-8"))
-            ])
-        else:
-            format_id = response_json["format_id"]
-            format_ext = response_json["ext"]
-            cb_string = "{}|{}|{}".format(
-                "file", format_id, format_ext, randem)
-            inline_keyboard.append([
-                InlineKeyboardButton(
-                    "unknown video format", callback_data=cb_string.encode("UTF-8"))
-            ])
-        reply_markup = InlineKeyboardMarkup(inline_keyboard)
-        logger.info(reply_markup)
-
-       # reply_markup = InlineKeyboardMarkup(inline_keyboard)
-
-        
-        await chk.delete()
-        await bot.send_message(
-            chat_id=update.chat.id,
-            text=Translation.FORMAT_SELECTION.format(Thumbnail) + "\n" + Translation.SET_CUSTOM_USERNAME_PASSWORD,
-            reply_markup=reply_markup,
-            parse_mode=enums.ParseMode.HTML,
-            reply_to_message_id=update.id
-        )
 
