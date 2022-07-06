d614029c2c7d443b810e13d145c847eebddf17af
TellyHub <106040852+TellyHub@users.noreply.github.com>
2022-07-06 20:33:43 +0530

    Update echo.py

--- plugins/echo.py
+++ plugins/echo.py
@@ -184,9 +184,47 @@ async def echo(bot, update):
                 if "filesize" in formats:
                     approx_file_size = humanbytes(formats["filesize"])
 
+                cb_string_video = "{}|{}|{}|{}".format(
+                    "video", format_id, format_ext, randem)
 
+                if not "audio only" in format_string:
+                    ikeyboard = [
+                        InlineKeyboardButton(
+                            "🎬 " + format_string + " " + format_ext + " " + approx_file_size + " ",
+                            callback_data=(cb_string_video).encode("UTF-8")
+                        )
+                    ]
 
-        reply_markup = pyrogram.InlineKeyboardMarkup(inline_keyboard)
+                else:
+                    ikeyboard = [
+                        InlineKeyboardButton(
+                            "🎬 [" +
+                            "] ( " +
+                            approx_file_size + " )",
+                            callback_data=(cb_string_video).encode("UTF-8")
+                        )
+                    ]
+                inline_keyboard.append(ikeyboard)
+            if duration is not None:
+                cb_string_64 = "{}|{}|{}|{}".format("audio", "64k", "mp3", randem)
+                cb_string_128 = "{}|{}|{}|{}".format("audio", "128k", "mp3", randem)
+                cb_string = "{}|{}|{}|{}".format("audio", "320k", "mp3", randem)
+                inline_keyboard.append([
+                    InlineKeyboardButton(
+                        "🎵 ᴍᴘ𝟹 " + "(" + "64 ᴋʙᴘs" + ")", callback_data=cb_string_64.encode("UTF-8")),
+                    InlineKeyboardButton(
+                        "🎵 ᴍᴘ𝟹 " + "(" + "128 ᴋʙᴘs" + ")", callback_data=cb_string_128.encode("UTF-8"))
+                ])
+                inline_keyboard.append([
+                    InlineKeyboardButton(
+                        "🎵 ᴍᴘ𝟹 " + "(" + "320 ᴋʙᴘs" + ")", callback_data=cb_string.encode("UTF-8"))
+                ])
+                inline_keyboard.append([                 
+                    InlineKeyboardButton(
+                        "✔️ ᴄʟᴏsᴇ", callback_data='close')               
+                ])
+
+        #reply_markup = pyrogram.InlineKeyboardMarkup(inline_keyboard)
 
         reply_markup = InlineKeyboardMarkup(inline_keyboard)
 
