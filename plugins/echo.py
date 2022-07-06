b3d1a2ee12f216ca6f0ab5f9725f82eacf259ada
TellyHub <106040852+TellyHub@users.noreply.github.com>
2022-07-06 19:01:08 +0530

    Update echo.py

--- plugins/echo.py
+++ plugins/echo.py
@@ -181,19 +181,7 @@ async def echo(bot, update):
             approx_file_size = ""
             if "filesize" in formats:
                 approx_file_size = humanbytes(formats["filesize"])
-            cb_string = "{}|{}|{}".format(
-                  "video", format_id, format_ext, format_string, randem)
-            if not "audio only" in format_string:
-                ikeyboard = [
-                    InlineKeyboardButton(
-                        "[" + format_string +
-                        "] (" + format_ext + " - " +
-                        approx_file_size + ")",
-                        callback_data=(cb_string).encode("UTF-8")
 
-                )
-            ]
-            inline_keyboard.append(ikeyboard)
         cb_string = "{}|{}|{}".format("audio", "5", "mp3")
         inline_keyboard.append([
             InlineKeyboardButton(
